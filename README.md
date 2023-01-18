50e4751999789437e5d56fa86d234093680235f8
ethanlee <seungwoonlee90@gmail.com>
2023-01-18 21:30:03 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+![header](https://capsule-render.vercel.app/api?type=waving&&color=auto&height=200&section=header&text=COUSRERA✍🏻&fontSize=65)
+
+# streamlit_python
+
+> Build a Data Science Web App with Streamlit and Python
---
+++ streamlit_python/main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+DATA_URL = './data/Motor_Vehicle_Collisions_-_Crashes.csv'
+
+st.title("Motor Vehicle Collisions in NYC")
+st.markdown("This application is a Streamlit dashboard that can be used"
+            "to analyze motor vehicle collision in NYC 🗽"
+            )
+
+@st.cache(persist=True)
+def load_data(nrows) :
+    data = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[['CRASH_DATE', 'CRASH_TIME']])
+    data.dropna(subset=['LATITUDE', 'LONGITUDE'], inplace=True)
+    lowcase = lambda x : str(x).lower()
+    data.rename(lowcase, axis='columns', inplace=True)
+    data.rename(columns={'crash_date_crash_time' : 'date/time'}, inplace=True)
+    return data
+
+data = load_data(100000)
+
+if st.checkbox("Show Raw Data", False) :
+    st.subheader('Raw Data')
+    st.write(data)
